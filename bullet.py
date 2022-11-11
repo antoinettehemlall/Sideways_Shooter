0f448a94c37453b3c37a9cef6c2ec0e4e5cc99bb
antoinettehemlall <hemlall3000@gmail.com>
2022-11-11 09:14:40 -0500

    Fixed direction of bullets

--- bullet.py
+++ bullet.py
@@ -16,13 +16,13 @@ class Bullet(Sprite):
         self.rect.midright = ss_game.ship.rect.midright
 
         # Store the bullet's position as a decimal value.
-        self.y = float(self.rect.y)
+        self.x = float(self.rect.x)
 
     def update(self):
         """Move the bullet up the screen."""
-        self.y -= self.settings.bullet_speed
+        self.x += self.settings.bullet_speed
         #update the rect position
-        self.rect.y = self.y
+        self.rect.x = self.x
 
     def draw_bullet(self):
         """Draw the bullet to the screen."""
